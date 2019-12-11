d41e80b618e2eb102dbac2eb744a37e948f18bdc
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2019-12-11 15:55:05 -0700

    coommit

--- parakeet.js
+++ parakeet.js
@@ -1,30 +1,24 @@
 const HTTP = new XMLHttpRequest();
 const url = ' http://144.17.24.80:30081/api/trygram/createtrygrams'
 const url2 = 'http://www.gutenberg.org/cache/epub/';
-HTTP.open("POST", url);
-HTTP.send();
-
-HTTP.onreadystatechange = (e) => {
-    console.log(Http.responseText)
-}
 
 function trygram() {
     var start = document.getElementById("startInt").value;
     var numberof = document.getElementById("numberInt").value;
-    for(i=0; i<numberof;i++){
-        var urlnew = url2+start+"/pg"+start+".txt";
+    for (i = 0; i < numberof; i++) {
+        var urlnew = url2 + start + "/pg" + start + ".txt";
         start++;
-    
-    //window.location.href=urlnew;
-   var str = getText();
-    //window.alert("got" + start + "  " + numberof);
 
-    HTTP.open("POST", url);
-    HTTP.send(str);
-    HTTP.onreadystatechange = (e) => {
-        console.log(Http.responseText)
+        window.location.href = urlnew;
+        var str = getText();
+        //window.alert("got" + start + "  " + numberof);
+
+        HTTP.open("POST", url);
+        HTTP.send(str);
+        HTTP.onreadystatechange = (e) => {
+            console.log(Http.responseText)
+        }
     }
-}
 }
 
 function getText() {
@@ -33,7 +27,7 @@ function getText() {
         bodyScripts[i].remove();
     }
     var str = document.body.textContent;
-    document.body.innerHTML = '<pre>' + str + '</pre>';
-}  
+    return str;
+}
 
 
